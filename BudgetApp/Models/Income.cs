5bcfa867564b86ef4ef967d9affe07ba6dd5f198
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-04-26 22:34:52 -0500

    Created Income model

---
+++ BudgetApp/Models/Income.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BudgetApp.Models
+{
+    internal class Income
+    {
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        // Contributor -> prop income, Income income;
+        public Income() 
+        {
+            Name = string.Empty;
+            Amount = 0;
+        }
+    }
+}
