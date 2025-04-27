05dc347f2ea293d1aa0f7e7b5f0c519c4a1a4e72
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-04-26 22:46:30 -0500

    Created Contributor Model

---
+++ BudgetApp/Models/Contributor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BudgetApp.Models
+{
+    internal class Contributor
+    {
+        public string Name { get; set; }
+        public List<Income> Income { get; set; }
+
+        public Contributor(string name, List<Income> income) 
+        { 
+            Name = name;
+            Income = income;
+        }
+
+        public Contributor() { }
+
+    }
+}
