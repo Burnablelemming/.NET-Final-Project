1b9c57c1241f3697539358e6a7593771fbbbce1e
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-05-09 21:14:33 -0500

    Added Click events for all of the buttons

--- BudgetApp/Views/MainWindow.xaml.cs
+++ BudgetApp/Views/MainWindow.xaml.cs
@@ -23,11 +23,58 @@ namespace BudgetApp.Views
             _controller = new MainWindowController(this);
         }
 
-        private void AddContributor_Click(object sender, RoutedEventArgs e)
+        private void AddContributorButton_Click(object sender, RoutedEventArgs e)
         {
+            // Possible implementation:
+            // var dialog = new dialog();
+            // dialog.showView;
             _controller.AddContributor();
         }
 
+        private void RemoveContributorButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddExpenseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveExpenseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddIncomeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveIncomeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadBudgetButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveBudgetButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
         public ListBox GetContributorListBox()
         {
             return this.ContributorsListBox;
