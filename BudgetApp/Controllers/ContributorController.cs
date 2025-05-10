17a5d5a077f0c3425e97713c574ef7734c1019a6
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-05-09 19:55:10 -0500

    Added AddContributor method and a pseudo implementation for how it will function.

--- BudgetApp/Controllers/ContributorController.cs
+++ BudgetApp/Controllers/ContributorController.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using BudgetApp.Models;
+using BudgetApp.Views;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace BudgetApp.Controllers
 {
     class ContributorController
     {
+        private ObservableCollection<Contributor> _contributors = new ObservableCollection<Contributor>();
+        private ListBox _contributorsListBox;
+        private AddContributorDialog _view = new AddContributorDialog();
+
+        public ContributorController(ListBox contributorsList)
+        {
+            _contributorsListBox = contributorsList;
+            _contributorsListBox.ItemsSource = _contributors;
+        }
+
+        public void AddContributor() {
+
+            // if _view.IsValid()
+            Contributor c = new Contributor();
+            //c.name = _view.ContributorNameTextBox.Text;
+            //contributors.add(c);
+            // close dialog
+        }
+
+
     }
 }
